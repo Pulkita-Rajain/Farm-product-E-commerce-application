7553a131300879135b00920fc37609d77e997947
Pulkita <pulkitarajain14@gmail.com>
2026-06-09 10:48:05 +0530

    updated usercontroller

--- ecom-application/src/main/java/com/app/ecom/UserController.java
+++ ecom-application/src/main/java/com/app/ecom/UserController.java
@@ -14,15 +14,16 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("/api/users")
+    @GetMapping
     public ResponseEntity<List<User>> getAllUsers(){
 
         return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
     }
-    @GetMapping("/api/users/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable Long id){
 
         return userService.fetchUser(id)
@@ -31,12 +32,12 @@ public class UserController {
     }
 
 
-    @PostMapping("/api/register")
+    @PostMapping
     public ResponseEntity<String> User(@RequestBody User user){
         userService.addUser(user);
         return ResponseEntity.ok("User added successfully");
     }
-    @PutMapping("/api/register/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody User updateUser){
         boolean updated = userService.updateUser(id, updateUser);
         if(updated)
